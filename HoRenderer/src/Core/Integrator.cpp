6947aba9c84177c30175a8311a96d4f7020f2ee0
Yinghao Ho <75870876+CherrySama@users.noreply.github.com>
2025-05-18 00:05:20 +0100

    add a ray into scene

--- HoRenderer/src/Core/Integrator.cpp
+++ HoRenderer/src/Core/Integrator.cpp
@@ -17,18 +17,45 @@ Integrator::~Integrator()
 
 void Integrator::RenderImage()
 {    
+    // 计算视口尺寸和相关参数
+    double aspect_ratio = double(width) / height;
+    double viewport_width = viewport_height * aspect_ratio;
+    
+    // 计算视口基向量
+    Vector3f viewport_u(viewport_width, 0, 0);
+    Vector3f viewport_v(0, -viewport_height, 0);
+    
+    // 计算像素间隔向量
+    Vector3f pixel_delta_u = viewport_u / float(width);
+    Vector3f pixel_delta_v = viewport_v / float(height);
+    
+    // 计算视口左上角位置
+    Vector3f viewport_upper_left = camera_center - Vector3f(0, 0, focal_length) - viewport_u/2.0f - viewport_v/2.0f;
+    
+    // 计算第一个像素的中心
+    Vector3f pixel00_loc = viewport_upper_left + 0.5f * (pixel_delta_u + pixel_delta_v);
+
     // Generate Image
      for (int j = 0; j < height; ++j) {
         uint8_t* row = pixels + j * width * 4;  // Pointer to the start of the line
         std::clog << "\rScanlines remaining: " << (height - j) << ' ' << std::flush;
         for (int i = 0; i < width; i++) {
-            auto r = double(i) / (width-1);
-            auto g = double(j) / (height-1);
-            auto b = 0.0;
+            // auto r = double(i) / (width-1);
+            // auto g = double(j) / (height-1);
+            // auto b = 0.0;
+            // 计算像素中心位置
+            Vector3f pixel_center = pixel00_loc + (float(i) * pixel_delta_u) + (float(j) * pixel_delta_v);
+            
+            // 创建射线：从相机中心到像素
+            Vector3f ray_direction = pixel_center - camera_center;
+            Ray r(camera_center, ray_direction);
+            
+            // 获取射线颜色
+            Vector3f color = ray_color(r);
 
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
+            int ir = int(255.999 * color.r);
+            int ig = int(255.999 * color.g);
+            int ib = int(255.999 * color.b);
 
             uint8_t* pixel = row + i * 4;  // Pointer to the current pixel
             pixel[0] = ir;  // R
--- HoRenderer/src/Core/Integrator.hpp
+++ HoRenderer/src/Core/Integrator.hpp
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "Util.hpp"
+#include "Ray.hpp"
 
 class Integrator{
 public:
@@ -12,6 +13,21 @@ public:
 
     void RenderImage();
     uint8_t *GetPixels();
+
+    // 简单相机参数
+    Vector3f camera_center{0, 0, 0};  // 相机位置
+    double viewport_height = 2.0;     // 视口高度
+    double focal_length = 1.0;        // 焦距
+
+    // 计算射线颜色（背景）
+    Vector3f ray_color(const Ray& r) {
+        // 获取射线的单位方向向量
+        Vector3f unit_direction = glm::normalize(r.direction());
+        // 基于y分量做线性插值，范围从0到1
+        float t = 0.5 * (unit_direction.y + 1.0);
+        // 从白色(1,1,1)到蓝色(0.5,0.7,1.0)的线性插值
+        return (1.0f - t) * Vector3f(1.0, 1.0, 1.0) + t * Vector3f(0.5, 0.7, 1.0);
+    }
 
 private:
     int width, height;

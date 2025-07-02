dee4059c90764e6a3389040b5ef812941f1c7738
Yinghao Ho <75870876+CherrySama@users.noreply.github.com>
2025-07-02 20:29:00 +0100

    completed QuadAreaLight

--- HoRenderer/src/Core/Light.cpp
+++ HoRenderer/src/Core/Light.cpp
@@ -1,3 +1,77 @@
 /*
     Created by Yinghao He on 2025-06-22
 */
+#include "Light.hpp"
+#include "Sampler.hpp"
+
+
+LightSampleInfo QuadAreaLight::Sample(const Vector3f& surface_pos, Sampler& sampler) const {
+    LightSampleInfo result;
+
+    Vector3f light_point = SampleQuadSurface(sampler);
+    Vector3f light_to_surface = light_point - surface_pos;
+    float distance_sq = glm::dot(light_to_surface, light_to_surface);
+    
+    if (distance_sq < Epsilon) {
+        result.valid = false;
+        return result;
+    }
+    
+    result.distance = std::sqrt(distance_sq);
+    result.direction = light_to_surface / result.distance;
+    
+    Vector3f light_normal = GetQuadNormal();
+    float cos_theta = glm::dot(-result.direction, light_normal);
+    
+    if (cos_theta <= 0.0f) {
+        result.valid = false;
+        return result;
+    }
+
+    result.pdf = distance_sq / (area * cos_theta);
+    result.radiance = color * intensity;
+    result.valid = true;
+    
+    return result;
+}
+
+Vector3f QuadAreaLight::Evaluate(const Vector3f& surface_pos, const Vector3f& light_dir, float& pdf) const {
+    Ray ray(surface_pos, light_dir);
+    Hit_Payload rec;
+    
+    if (!quad->isHit(ray, Vector2f(Epsilon, Infinity), rec)) {
+        pdf = 0.0f;
+        return Vector3f(0);
+    }
+    
+    Vector3f light_normal = GetQuadNormal();
+    float cos_theta = glm::dot(-light_dir, light_normal);
+    
+    if (cos_theta <= 0.0f) {
+        pdf = 0.0f;
+        return Vector3f(0);
+    }
+    
+    float distance_sq = rec.t * rec.t;
+    pdf = distance_sq / (area * cos_theta);
+    
+    return color * intensity;
+}
+
+bool QuadAreaLight::IsHit(const Ray& ray, float max_distance, Vector3f& radiance) const {
+    Hit_Payload rec;
+    if (quad->isHit(ray, Vector2f(Epsilon, max_distance), rec)) {
+        radiance = color * intensity;
+        return true;
+    }
+    return false;
+}
+
+Vector3f QuadAreaLight::SampleQuadSurface(Sampler& sampler) const {
+    Vector2f uv = sampler.get_2d_sample();
+    return quad->get_Q() + uv.x * quad->get_u() + uv.y * quad->get_v();
+}
+
+Vector3f QuadAreaLight::GetQuadNormal() const {
+    return glm::normalize(glm::cross(quad->get_u(), quad->get_v()));
+}
--- HoRenderer/src/Core/Light.hpp
+++ HoRenderer/src/Core/Light.hpp
@@ -1,3 +1,92 @@
 /*
     Created by Yinghao He on 2025-06-22
 */
+#pragma once
+
+#include "Util.hpp"
+#include "Shape.hpp"
+
+struct LightSampleInfo {
+    Vector3f radiance;      
+    Vector3f direction;     
+    float distance;         
+    float pdf;              
+    bool valid;             
+    
+    LightSampleInfo() : radiance(0), direction(0), distance(Infinity), pdf(0), valid(false) {}
+};
+
+class Light {
+public:
+    virtual ~Light() = default;
+
+    virtual LightSampleInfo Sample(const Vector3f& surface_pos, Sampler& sampler) const = 0;
+    virtual Vector3f Evaluate(const Vector3f& surface_pos, const Vector3f& light_dir, float& pdf) const = 0;
+    virtual bool IsHit(const Ray &ray, float max_distance, Vector3f &radiance) const = 0;
+
+    virtual float GetPower() const { return power; }
+
+protected:
+    Vector3f intensity;     
+    Vector3f color;         
+    float power;
+};
+
+class QuadAreaLight : public Light {
+public:
+    QuadAreaLight(std::shared_ptr<Quad> quad, const Vector3f &color, float intensity = 1.0f) :
+        quad(quad) {
+        this->color = color;
+        this->intensity = Vector3f(intensity);
+        area = glm::length(glm::cross(quad->get_u(), quad->get_v()));
+        power = glm::length(color) * intensity * area * PI;
+    }
+
+    virtual LightSampleInfo Sample(const Vector3f& surface_pos, Sampler& sampler) const override;
+    virtual Vector3f Evaluate(const Vector3f &surface_pos, const Vector3f &light_dir, float &pdf) const override;
+    virtual bool IsHit(const Ray &ray, float max_distance, Vector3f &radiance) const override;
+
+private:
+    Vector3f SampleQuadSurface(Sampler &sampler) const;
+    Vector3f GetQuadNormal() const;
+
+private:
+    std::shared_ptr<Quad> quad;
+    float area;
+};
+
+class SphereAreaLight : public Light {
+public:
+    SphereAreaLight(std::shared_ptr<Sphere> sphere, const Vector3f &color, float intensity = 1.0f);
+
+    virtual LightSampleInfo Sample(const Vector3f &surface_pos, Sampler &sampler) const override;
+    virtual Vector3f Evaluate(const Vector3f &surface_pos, const Vector3f &light_dir, float &pdf) const override;
+    virtual bool IsHit(const Ray &ray, float max_distance, Vector3f &radiance) const override;
+
+private:
+    Vector3f SampleSphereSurface(Sampler &sampler) const;
+    bool SampleSphereDirection(const Vector3f &surface_pos, Sampler &sampler, Vector3f &direction, float &pdf) const;
+
+private:
+    std::shared_ptr<Sphere> sphere;
+    Vector3f center;
+    float radius;
+    float area;
+};
+
+class InfiniteAreaLight : public Light {
+public:
+    InfiniteAreaLight(std::shared_ptr<HDRTexture> hdr, float scale = 1.0f);
+
+    virtual LightSampleInfo Sample(const Vector3f& surface_pos, Sampler& sampler) const override;
+    virtual Vector3f Evaluate(const Vector3f& surface_pos, const Vector3f& light_dir, float& pdf) const override;
+    virtual bool IsHit(const Ray& ray, float max_distance, Vector3f& radiance) const override;
+
+private:
+    Vector3f SphericalToCartesian(float theta, float phi) const;
+    Vector2f CartesianToSpherical(const Vector3f &dir) const;
+
+private:
+    std::shared_ptr<HDRTexture> hdr_texture;
+    float scale;
+};

dc33d02258b5a0fe6efb152147bf503a3d5d3bb7
Yinghao Ho <75870876+CherrySama@users.noreply.github.com>
2025-08-12 17:30:59 +0800

    Update RendererScene.cpp

--- HoRenderer/src/Core/RendererScene.cpp
+++ HoRenderer/src/Core/RendererScene.cpp
@@ -121,48 +121,4 @@ namespace RendererScene
         auto renderer = std::make_shared<Renderer>(std::move(camera), std::move(integrator), std::move(sampler), std::move(scene));
         return renderer;
     }
-
-    std::shared_ptr<Renderer> TestScene()
-    {
-        CameraParams camParams = {16.0f / 9.0f,
-                                  1600,
-                                  40.0f,
-                                  Vector3f(-200.0f, 50.0f, 0.0f),
-                                  Vector3f(-700.0f, 50.0f, -50.0f),
-                                  Vector3f(0.0f, 1.0f, 0.0f),
-                                  0.0f,
-                                  1.0f,
-                                  0};
-        std::unique_ptr<Camera> camera = std::make_unique<Camera>();
-        camera->Create(camParams);
-
-        std::unique_ptr<Integrator> integrator = std::make_unique<Integrator>(camera->image_width, camera->image_height, 12, 30);
-        std::unique_ptr<Sampler> sampler = std::make_unique<Sampler>(FilterType::GAUSSIAN);
-        std::unique_ptr<Scene> scene = std::make_unique<Scene>();
-
-        auto whiteMaterial = std::make_shared<Diffuse>(Vector3f(0.73f, 0.73f, 0.73f));
-        scene->Add(std::make_shared<Quad>(Vector3f(-800.0f, 0.0f, -200.0f),
-                                          Vector3f(600.0f, 0.0f, 0.0f),
-                                          Vector3f(0.0f, 0.0f, 400.0f),
-                                          Transform(),
-                                          whiteMaterial));
-
-        scene->Add(std::make_shared<Box>(Vector3f(-400.0f, 25.0f, -25.0f),
-                                         Vector3f(50.0f, 50.0f, 50.0f),
-                                         Transform(),
-                                         whiteMaterial));
-
-        // Env Light
-        auto fm = FileManager::getInstance();
-        fm->init();
-        auto hdr_texture = std::make_shared<HDRTexture>(fm->getEnvBGPath("circus_arena_4k.hdr").c_str());
-        auto env_light = std::make_shared<InfiniteAreaLight>(hdr_texture, 3.0f);
-        scene->AddEnvLight(env_light);
-
-        scene->BuildBVH();
-        scene->BuildLightTable();
-
-        auto renderer = std::make_shared<Renderer>(std::move(camera), std::move(integrator), std::move(sampler), std::move(scene));
-        return renderer;
-    }
 }

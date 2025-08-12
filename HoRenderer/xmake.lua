9b66000dc3f99d93d6428ced9c5779fe1251dbf9
Yinghao Ho <75870876+CherrySama@users.noreply.github.com>
2025-08-12 16:52:12 +0800

    Update xmake.lua

--- HoRenderer/xmake.lua
+++ HoRenderer/xmake.lua
@@ -1,6 +1,6 @@
 add_rules("mode.debug", "mode.release")
 
-add_requires("glfw", "glad", "glm", "embree", "nlohmann_json")
+add_requires("glfw", "glad", "glm", "embree", "nlohmann_json", "tinyobjloader")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = "../.vscode"})
 set_languages("c++23") 
 
@@ -21,7 +21,7 @@ target("HoRenderer")
         os.cp("Shader/*.frag", target:targetdir())
     end)
 
-    add_packages("glfw", "glad", "glm", "embree", "nlohmann_json")
+    add_packages("glfw", "glad", "glm", "embree", "nlohmann_json", "tinyobjloader")
 
     add_cxflags("/openmp:llvm")
 --
